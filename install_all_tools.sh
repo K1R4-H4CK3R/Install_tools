b442c291422d96275c5abaac33b02e74b0acf190
K1R4-H4CK3R <142159487+K1R4-H4CK3R@users.noreply.github.com>
2023-08-30 17:32:44 -0300

    Add files via upload

---
+++ install_all_tools.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+# Instalação das Ferramentas de Pentesting
+
+# Ferramentas Iniciais
+apt install nmap
+apt install metasploit-framework
+apt install wireshark
+apt install aircrack-ng
+apt install john
+apt install gobuster
+apt install hydra
+apt install sqlmap
+
+# Ferramentas Adicionais
+apt install dirsearch
+apt install sublist3r
+apt install gitleaks
+# Nessus: Baixe do site oficial.
+apt install wpscan
+apt install dirb
+# Recon-ng: Já instalado, mas pode ser executado com recon-ng.
+apt install beef-xss
+apt install snort
+# Nikto: Já instalado, mas pode ser executado com nikto.
+apt install aquatone
+apt install wfuzz
+apt install sn1per
+apt install brutespray
+apt install crackmapexec
+# Subfinder: Já instalado, mas pode ser executado com subfinder.
+apt install dnsrecon
+apt install wafw00f
+apt install sublist3r
+# SecLists: Já instalado, mas pode ser acessado na pasta /usr/share/seclists.
+apt install bbqsql
+apt install fruitywifi
+apt install radare2
+apt install jd-gui
+# Sn1per: Já instalado, mas pode ser executado com sniper.
+apt install subfinder
+apt install yersinia
+# Nessus: Baixe do site oficial.
+apt install cr3d0v3r
+apt install davtest
+apt install lynis
+apt install masscan
+apt install joomscan
+apt install shellnoob
+apt install autorecon
+apt install striker
+apt install sniffjoke
+apt install joomscan
+apt install ghost-phisher
+apt install ruler
+# Recon-ng: Já instalado, mas pode ser executado com recon-ng.
+apt install owasp-zsc
+apt install wpscan
+apt install xsser
+apt install set
+apt install w3af
+# ZAP (OWASP Zed Attack Proxy): Já instalado, mas pode ser executado com zaproxy.
+apt install raccoon
+# Armitage: Já instalado, mas pode ser executado com armitage.
+apt install metagoofil
+apt install whatweb
+# Hydra: Já instalado, mas pode ser executado com hydra.
+# Hashcat: Já instalado, mas pode ser executado com hashcat.
+
+echo "Instalação concluída!"
